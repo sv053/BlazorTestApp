e722045488597582dd82f272455b5d2f8baef440
scenacheros <scenacheros@yandex.ru>
2021-02-20 18:22:49 +0100

    examples are added

--- BlazorTestApp/Startup.cs
+++ BlazorTestApp/Startup.cs
@@ -29,6 +29,8 @@ namespace BlazorTestApp
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
+            services.AddTelerikBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
